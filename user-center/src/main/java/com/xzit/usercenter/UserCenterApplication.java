d8093bc728a0772ba3131b206540d322a2f9c3ca
lwnp <2300362038@qq.com>
2024-02-02 12:53:46 +0800

    创建项目

--- user-center/src/main/java/com/xzit/usercenter/UserCenterApplication.java
+++ user-center/src/main/java/com/xzit/usercenter/UserCenterApplication.java
@@ -3,11 +3,9 @@ package com.xzit.usercenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication(scanBasePackages = {"com.xzit"})
-@EnableFeignClients(basePackages = {"com.xzit.api.**.feign"})
+@EnableFeignClients(basePackages = {"com.xzit.api.**"})
 public class UserCenterApplication {
 
     public static void main(String[] args) {
