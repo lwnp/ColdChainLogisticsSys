496607aa32c106b5b0577c1b4dbcb54bc9e4cd18
lwnp <2300362038@qq.com>
2024-02-06 15:34:33 +0800

    修正项目

---
+++ sys-authorization/src/main/java/com/xzit/sysauthorization/controller/ConsentController.java
@@ -0,0 +1,117 @@
+package com.xzit.sysauthorization.controller;
+
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.*;
+
+@Controller
+public class ConsentController {
+    private final RegisteredClientRepository registeredClientRepository;
+    private final OAuth2AuthorizationConsentService authorizationConsentService;
+
+    public ConsentController(RegisteredClientRepository registeredClientRepository,
+                                          OAuth2AuthorizationConsentService authorizationConsentService) {
+        this.registeredClientRepository = registeredClientRepository;
+        this.authorizationConsentService = authorizationConsentService;
+    }
+
+    @GetMapping(value = "/oauth2/consent")
+    public String consent(Principal principal, Model model,
+                          @RequestParam(OAuth2ParameterNames.CLIENT_ID) String clientId,
+                          @RequestParam(OAuth2ParameterNames.SCOPE) String scope,
+                          @RequestParam(OAuth2ParameterNames.STATE) String state) {
+
+        // Remove scopes that were already approved
+        Set<String> scopesToApprove = new HashSet<>();
+        Set<String> previouslyApprovedScopes = new HashSet<>();
+        RegisteredClient registeredClient = this.registeredClientRepository.findByClientId(clientId);
+        OAuth2AuthorizationConsent currentAuthorizationConsent =
+                null;
+        if (registeredClient != null) {
+            currentAuthorizationConsent = this.authorizationConsentService.findById(registeredClient.getId(), principal.getName());
+        }
+        Set<String> authorizedScopes;
+        if (currentAuthorizationConsent != null) {
+            authorizedScopes = currentAuthorizationConsent.getScopes();
+        } else {
+            authorizedScopes = Collections.emptySet();
+        }
+        for (String requestedScope : StringUtils.delimitedListToStringArray(scope, " ")) {
+            if (OidcScopes.OPENID.equals(requestedScope)) {
+                continue;
+            }
+            if (authorizedScopes.contains(requestedScope)) {
+                previouslyApprovedScopes.add(requestedScope);
+            } else {
+                scopesToApprove.add(requestedScope);
+            }
+        }
+
+        model.addAttribute("clientId", clientId);
+        if (registeredClient != null) {
+            model.addAttribute("clientName",registeredClient.getClientName());
+        }
+        model.addAttribute("state", state);
+        model.addAttribute("scopes", withDescription(scopesToApprove));
+        model.addAttribute("previouslyApprovedScopes", withDescription(previouslyApprovedScopes));
+        model.addAttribute("principalName", principal.getName());
+        model.addAttribute("requestURI", "/oauth2/authorize");
+        if (registeredClient != null) {
+            model.addAttribute("redirectUri",registeredClient.getRedirectUris().iterator().next());
+        }
+
+
+        return "consent";
+    }
+
+    private static Set<ScopeWithDescription> withDescription(Set<String> scopes) {
+        Set<ScopeWithDescription> scopeWithDescriptions = new HashSet<>();
+        for (String scope : scopes) {
+            scopeWithDescriptions.add(new ScopeWithDescription(scope));
+
+        }
+        return scopeWithDescriptions;
+    }
+
+    public static class ScopeWithDescription {
+        private static final String DEFAULT_DESCRIPTION = "UNKNOWN SCOPE - We cannot provide information about this permission, use caution when granting this.";
+        private static final Map<String, String> scopeDescriptions = new HashMap<>();
+        static {
+            scopeDescriptions.put(
+                    OidcScopes.PROFILE,
+                    "授权应用读取你的个人资料"
+            );
+            scopeDescriptions.put(
+                    "message.read",
+                    "授权应用读取信息"
+            );
+            scopeDescriptions.put(
+                    "message.write",
+                    "授权应用写入信息"
+            );
+            scopeDescriptions.put(
+                    "other.scope",
+                    "其他授权"
+            );
+        }
+
+        public final String scope;
+        public final String description;
+
+        ScopeWithDescription(String scope) {
+            this.scope = scope;
+            this.description = scopeDescriptions.getOrDefault(scope, DEFAULT_DESCRIPTION);
+        }
+    }
+}
