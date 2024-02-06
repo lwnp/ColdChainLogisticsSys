6f1fcc7e9f37ccea6293a4bd92fb42ca873bc7ee
lwnp <2300362038@qq.com>
2024-02-06 15:32:19 +0800

    修正项目

--- sys-authorization/src/main/java/com/xzit/sysauthorization/constant/AuthorizationConstant.java
+++ sys-authorization/src/main/java/com/xzit/sysauthorization/constant/AuthorizationConstant.java
@@ -9,6 +9,8 @@ public interface AuthorizationConstant {
     String ENCODE_SECRETE="marshmello";                              //密钥
     String CLIENT_ID="system-client";                                 //client 也是本系统的网关
     String AUTHORIZATION_ENDPOINT="http://127.0.0.1:9999";          //认证服务端点
+    String CLIENT_REDIRECT_URI="http://127.0.0.1:8888/login/oauth2/code/system-client-oidc";
+    String POST_LOGOUT_URI="http://127.0.0.1:8888/logout";
 
 
 }
