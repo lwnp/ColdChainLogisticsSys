cce53dacc576d1c702e630d6db37e0bbc4c95e9f
lwnp <2300362038@qq.com>
2024-02-25 20:41:14 +0800

    修正项目

---
+++ common/common-doc/src/main/java/com/xzit/commondoc/CommonDocApplication.java
@@ -0,0 +1,13 @@
+package com.xzit.commondoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonDocApplication.class, args);
+    }
+
+}
---
+++ common/common-doc/src/test/java/com/xzit/commondoc/CommonDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzit.commondoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
