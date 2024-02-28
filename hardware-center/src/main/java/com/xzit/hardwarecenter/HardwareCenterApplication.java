18c30306502d03a827be76d8ca0e1c60b0b7ae5b
lwnp <2300362038@qq.com>
2024-02-28 21:53:13 +0800

    添加password模式

---
+++ hardware-center/src/main/java/com/xzit/hardwarecenter/HardwareCenterApplication.java
@@ -0,0 +1,13 @@
+package com.xzit.hardwarecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareCenterApplication.class, args);
+    }
+
+}
---
+++ hardware-center/src/test/java/com/xzit/hardwarecenter/HardwareCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzit.hardwarecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
