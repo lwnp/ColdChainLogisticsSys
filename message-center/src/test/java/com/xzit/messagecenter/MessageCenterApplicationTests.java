29ec28a525c7fb0ac1264201447ecf18fe0c126c
lwnp <2300362038@qq.com>
2024-03-03 17:00:27 +0800

    注册功能

---
+++ message-center/src/test/java/com/xzit/messagecenter/MessageCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzit.messagecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
